9799925d7053cfaff7a55cfa77591a4624383c73
nwang123 <87619999+nwang123@users.noreply.github.com>
2024-10-03 18:30:43 -0400

    Update data simulation example.R

--- example/data simulation example.R
+++ example/data simulation example.R
@@ -3,8 +3,7 @@
 This repository contains an example of simulating gene expression data with various biological parameters using R. The code simulates data for two groups with differing sample sizes and includes variance parameters for noise and effect size simulation.
 ## Simulation Overview
 
-This simulation generates gene expression or SNP data for two groups (`n1 = 300`, `n2 = 600`), and it simulates 180 genes with varying numbers of SNPs (5 SNPs per gene). The true effect sizes for these SNPs are controlled by the `truealpha` parameter.
-
+This simulation generates gene expression or SNP data for reference panel (`n1 = 300`) and GWAS data (`n2 = 600`), and it simulates 180 genes with varying numbers of SNPs (5 SNPs per gene). The true effect sizes for these SNPs are controlled by the `truealpha` parameter.
 ### Parameters:
 - **n1**: Number of samples in the first group (e.g., 300 ).
 - **n2**: Number of samples in the second group (e.g., 600 ).
@@ -13,6 +12,7 @@ This simulation generates gene expression or SNP data for two groups (`n1 = 300`
 - **k**: SNP loadings for each gene. Some groups have higher impact SNPs (`sqrt(25)`), while others have smaller effects.
 - **sigma1**, **sigma2**, **sigmau**: Variance components for noise in the data.
 - **truealpha**: A vector representing the true effect sizes of the SNPs.
+
 
 n1 = 300
 n2 = 600
